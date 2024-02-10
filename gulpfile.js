d06eb2088dbbeb8cfde6de005c595a91bd39414f
Екатерина Смирнова <kacayun.mne@gmail.com>
2024-02-10 14:07:25 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+const gulp = require('gulp');
+const requireDir = require('require-dir');
+const tasks = requireDir('./tasks');
+
+exports.style = tasks.style;
+exports.html = tasks.html;
+exports.watch = tasks.watch;
+
+exports.default = gulp.parallel(
+	exports.style,
+	exports.html,
+	exports.watch
+)
